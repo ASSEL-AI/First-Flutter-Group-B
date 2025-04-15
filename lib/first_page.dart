76802fa8a16c96bf4c8eabb70c7893e86108b3d4
ASSEL ALMALKI <157348946+ASSEL-AI@users.noreply.github.com>
2025-04-15 22:53:54 +0300

    flutter Task1 - Assel Almalki

---
+++ lib/first_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'second_page.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondPage()),
+            );
+          },
+          child: const Text('Go to Second Page'),
+        ),
+      ),
+    );
+  }
+}
